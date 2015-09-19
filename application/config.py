e44c999d74ce1f851d3ab7056cda234b4958da4c
xrlin <professerlin@gmail.com>
2015-09-19 20:51:56 +0800

    Update config.py

--- application/config.py
+++ application/config.py
@@ -19,11 +19,11 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "date.sqlite")
 WHOOSH_BASE = os.path.join(basedir, "whoosh_base")   # 设定全文搜索的数据库
 # mail configureation
 MAIL_USE_SSL = True
-MAIL_SERVER = 'smtp.exmail.qq.com'
+MAIL_SERVER = 'xx'
 MAIL_PORT = 465
-MAIL_USERNAME = 'xrlin@m.scnu.edu.cn'
-MAIL_PASSWORD = 'asd1079316345+'
-MAIL_DEFAULT_SENDER = 'xrlin@m.scnu.edu.cn'     # 默认的sender
+MAIL_USERNAME = 'xx'
+MAIL_PASSWORD = 'xx'
+MAIL_DEFAULT_SENDER = 'xx'     # 默认的sender
 
 SUBJECT_HEADER = "来自xrlin的验证信息"     # 邮件标题
 REPLY_SUBJECT_HEADER = "您在xrlin's blog的评论收到回复"  # 用作回复提醒的邮件标题
@@ -44,7 +44,7 @@ REPLY_MAIL_BODY_HTML = "<p><b>您好:</b></p>" \
                  "<p>Send from Xrlin's Blog</p>"
 
 
-SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+SECRET_KEY = 'xxxxx'
 
 UPLOAD_FOLDER = './static/upload/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -53,4 +53,4 @@ RECAPTCHA_PUBLIC_KEY = '6Ld4rwITAAAAAKUD5AntlHi7HL36W2vHJQOIjQmA'
 RECAPTCHA_PRIVATE_KEY = '6Ld4rwITAAAAAFE8nTS852QbsqCBx1mN8D4BqenE'
 
 # 管理员帐号名， 为配置方便暂时将管理员用户名写在配置文件
-Admin = "xrlin"
+Admin = "xx"
